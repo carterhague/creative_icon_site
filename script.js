272ae61bbe9c60a37bf8098c77973fc77e0e18fe
Carter Hague <me@fakeemail.com>
2020-10-02 19:14:40 -0600

    testing api

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ document.getElementById("submit").addEventListener("click", function(event) {
     return;
   console.log(value);
   console.log("woot woot");
+
+  const data = null;
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function() {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("GET", "http://api.iconfinder.com/v4/icons/search?query=" + value);
+  xhr.setRequestHeader("authorization", "Bearer l2qDH6G3EcsadPndGGDB013TUTttx8mQEqq8UkCKqJ7C0OefM4TOmbyPsyN1nLX0");
+  xhr.send(data);
 });
